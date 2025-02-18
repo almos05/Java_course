c522fe39f64bcb5f3d181db64e8c9df9a709a428
Мосолов Александр Денисович <1132236128@pfur.ru>
2025-02-18 17:46:50 +0300

    add some moves

--- src/App.java
+++ src/App.java
@@ -17,14 +17,30 @@ public class App extends JFrame {
         label.setOpaque(true);
         this.add(label);
 
+        JLabel imageClown = new JLabel(new ImageIcon("src/images/clown.png"));
+        imageClown.setBounds(20, 110, 116, 90); // размер клоуна 116x90
+        this.add(imageClown);
+
         JButton button = new JButton("Click");
         button.setBounds(20, 50, 200, 60);
         button.setBackground(Color.BLACK);
         button.setForeground(Color.WHITE); // цвет текста
         add(button);
 
-        MyButtonListener listener = new MyButtonListener(button);
+        //MyButtonListener listener = new MyButtonListener(button, 10);
+        MyButtonListener listener = new MyButtonListener(imageClown, 52); // используем image, чтобы именно эта компонента уезжала
         button.addActionListener(listener);
+
+        // JButton button2 = new JButton("Click"); // Вторая кнопка
+        // button2.setBounds(20, 110, 200, 60);
+        // button2.setBackground(Color.BLACK);
+        // button2.setForeground(Color.WHITE); // цвет текста
+        // add(button2);
+
+        // MyButtonListener listener2 = new MyButtonListener(button2);
+        // button2.addActionListener(listener2);
+        // button2.addActionListener(listener);
+
         //button.addActionListener((ActionEvent e) -> {}); // lambda функция
 
         setVisible(true); // Вывод окна
--- src/MyButtonListener.java
+++ src/MyButtonListener.java
@@ -1,17 +1,25 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
+import javax.swing.JComponent;
 
 public class MyButtonListener implements ActionListener { // Добавляем класс для обработки нажатия кнопки
-    private JButton button;
+    //private JButton button;
+    private JComponent component; // меняем на JComponent (родительский класс)
     private int counter = 0;
+    private int dx;
 
-    public MyButtonListener(JButton button) {
-        this.button = button;
+    public MyButtonListener(JComponent component, int dx) {
+        this.component = component;
+        this.dx = dx;
     }
+
+    // @Override
+    // public void actionPerformed(ActionEvent e) {
+    //     this.button.setText("Clicked " + ++counter); // АХХАХАХАХХАХАХАХАХХА (меняем счётчик)
+    // }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        this.button.setText("Clicked " + ++counter); // АХХАХАХАХХАХАХАХАХХА (меняем счётчик)
+        this.component.setLocation(component.getX() + this.dx, component.getY()); // АХХАХАХАХХАХАХАХАХХА (меняем счётчик)
     }
 }
